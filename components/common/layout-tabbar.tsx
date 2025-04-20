1310d595183e1e1e8c8f38c19cfa51f4ca315db6
tuanta@ichiba <tuanta@ichiba>
2025-04-20 17:56:27 +0700

    dismis keyboard

--- components/common/layout-tabbar.tsx
+++ components/common/layout-tabbar.tsx
@@ -1,16 +1,23 @@
 // MainLayout.tsx
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import CustomTabBar from "@/components/common/custom-tabbar";
 import HeaderTabs from "./header-tabs";
 
 const TabbarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <View style={styles.container}>
-      <HeaderTabs />
-      <View style={styles.content}>{children}</View>
-      <CustomTabBar />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <HeaderTabs />
+        <View style={styles.content}>{children}</View>
+        <CustomTabBar />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
